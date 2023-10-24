5e883c6b2ad6f79edd3d1d507adeb916c388ec74
evanf <evanfungv@gmail.com>
2023-10-24 17:29:29 -0400

    Refactor:Apply SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Card, Flex, Heading, Text} from "@radix-ui/themes";
+import IssueStatusBadge from "../../components/IssueStatusBadge";
+import ReactMarkdown from "react-markdown";
+import {Issue} from "@prisma/client";
+const IssueDetails = ({issue}: {issue:Issue}) => {
+    return (
+        <div>
+            <Heading>{issue.title}</Heading>
+            <Flex className='space-x-3' my='2'>
+                <IssueStatusBadge status={issue.status} />
+                <Text>{issue.createAt.toDateString()}</Text>
+            </Flex>
+            <Card className='prose' mt='4'>
+                <ReactMarkdown>{issue.description}</ReactMarkdown>
+            </Card>
+        </div>
+    );
+};
+
+export default IssueDetails;
---
+++ app/issues/[id]/edit/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssueDetailEditPage = () => {
+    return (
+        <div>
+            issue detail edit page
+        </div>
+    );
+};
+
+export default IssueDetailEditPage;
