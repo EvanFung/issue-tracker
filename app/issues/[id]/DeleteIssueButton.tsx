0f223a78a815aa3c846380d6cb5374cf990eb4c5
evanf <evanfungv@gmail.com>
2023-10-26 23:30:54 -0400

    Add spinner on delete button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,19 +3,23 @@ import {AlertDialog, Button, Flex} from "@radix-ui/themes";
 import axios from "axios";
 import {useRouter} from "next/navigation";
 import {useState} from "react";
+import Spinner from "@/app/components/Spinner";
 
 const DeleteIssueButton = ({issueId}: {
     issueId: number
 }) => {
     const router = useRouter();
     const [error, setError] = useState(false);
+    const [isDeleting, setDeleting] = useState(false);
     const deleteIssue = async () => {
         try {
+            setDeleting(true);
             await axios.delete('/api/issues/' + issueId);
             router.push('/issues');
             router.refresh();
         } catch (error) {
-            error = true;
+            setDeleting(false);
+            setError(true);
         }
     }
     return (
@@ -34,7 +38,7 @@ const DeleteIssueButton = ({issueId}: {
                             <Button variant='soft' color='gray'>Cancel</Button>
                         </AlertDialog.Cancel>
                         <AlertDialog.Action>
-                            <Button color='red' onClick={deleteIssue}>Delete Issue</Button>
+                            <Button color='red' onClick={deleteIssue} disabled={isDeleting}>Delete Issue{isDeleting && <Spinner />}</Button>
                         </AlertDialog.Action>
                     </Flex>
                 </AlertDialog.Content>
