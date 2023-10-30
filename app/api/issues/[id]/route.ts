c10d9295f98af784809cbe48947fb79cba7a2197
evanf <evanfungv@gmail.com>
2023-10-30 00:17:48 -0400

    Enhance the API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import {NextRequest, NextResponse} from "next/server";
-import {issueSchema} from "@/app/validationSchemas";
+import {patchIssueSchema} from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import {getServerSession} from "next-auth";
 import authOptions from "@/app/auth/authOptions";
@@ -20,10 +20,19 @@ export async function PATCH(
         }
     }) {
     const session = await getServerSession(authOptions);
-    if(!session)
-        return NextResponse.json({},{status: 401});
+    if (!session)
+        return NextResponse.json({}, {status: 401});
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const {assignedToUserId, title, description} = body;
+    if (assignedToUserId) {
+        const user = prisma.user.findUnique({
+            where: {id: assignedToUserId}
+        })
+        if (!user) {
+            return NextResponse.json({error: 'Invalid user.'}, {status: 400})
+        }
+    }
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success) {
         return NextResponse.json(validation.error.format(), {status: 400});
     }
@@ -35,8 +44,9 @@ export async function PATCH(
     const updatedIssue = await prisma.issue.update({
         where: {id: issue.id},
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId,
         }
     });
     return NextResponse.json(updatedIssue);
@@ -50,8 +60,8 @@ export async function DELETE(
         }
     }) {
     const session = await getServerSession(authOptions);
-    if(!session)
-        return NextResponse.json({},{status: 401});
+    if (!session)
+        return NextResponse.json({}, {status: 401});
     const issue = await prisma.issue.findUnique({
         where: {id: parseInt(params.id)}
     });
