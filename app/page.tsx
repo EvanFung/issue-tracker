85781a224cf13ede5f81ec0bb2f88294532de8a9
evanf <evanfungv@gmail.com>
2023-11-01 15:23:35 -0400

    Build the issue summary board

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
 import LatestIssues from "@/app/LatestIssues";
+import IssueSummary from "@/app/IssueSummary";
+import prisma from "@/prisma/client";
 
-export default function Home({searchParams}: {searchParams:{page: string}}) {
+export default async function Home() {
+    const open = await prisma.issue.count({
+        where:{
+            status:'OPEN'
+        }
+    });
+    const inProgress = await prisma.issue.count({
+        where:{
+            status:'IN_PROGRESS'
+        }
+    });
+    const closed = await prisma.issue.count({
+        where:{
+            status:'CLOSED'
+        }
+    });
   return (
     <div>
-        <LatestIssues />
+        {/*<LatestIssues />*/}
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
     </div>
   )
 }
